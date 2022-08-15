61ad1aba80e1818f452bf762030678b65f88ec67
alexmonteil <alex.monteil@outlook.com>
2022-08-15 01:14:57 -0500

    Handled no products case

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { Fragment, useEffect, useState } from "react";
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
@@ -6,12 +7,23 @@ import ProductList from "./ProductList";
 export default function Catalog() {
 
     const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetch("https://localhost:7092/api/products")
-        .then(response => response.json())
-        .then(data => setProducts(data));
+        axios.get('https://localhost:7092/api/products')
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false));
     }, []);
+
+
+    if (loading) {
+        return <h3>Loading...</h3>
+    }
+
+    if (products.length < 1) {
+        return <h3>No products found</h3>
+    }
 
     return (
         <Fragment>
