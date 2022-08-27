f3c26c8951eb04581f9f64fa8b5ef042c4624ed0
alexmonteil <alex.monteil@outlook.com>
2022-08-27 03:08:09 -0500

    implemented caching with redux toolkit

--- client/src/features/basket/basketSlice.ts
+++ client/src/features/basket/basketSlice.ts
@@ -13,9 +13,9 @@ const initialState: BasketState = {
     status: 'idle'
 }
 
-export const addBasketItemAsync = createAsyncThunk<Basket, {productId: number, quantity?: number }>(
+export const addBasketItemAsync = createAsyncThunk<Basket, {productId: number, quantity: number }>(
     'basket/addBasketItemAsync',
-    async ({productId, quantity = 1}) => {
+    async ({productId, quantity}) => {
 
         try {
             return await agent.Basket.addItem(productId, quantity);
@@ -25,6 +25,17 @@ export const addBasketItemAsync = createAsyncThunk<Basket, {productId: number, q
     }
 );
 
+export const removeBasketItemAsync = createAsyncThunk<void, {productId: number, quantity: number, name?: string}>(
+    'basket/removeBasketItemAsync',
+    async ({productId, quantity}) => {
+        try {
+            await agent.Basket.removeItem(productId, quantity);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+)
+
 
 export const basketSlice = createSlice({
     name: 'basket',
@@ -32,15 +43,6 @@ export const basketSlice = createSlice({
     reducers: {
         setBasket: (state, action) => {
             state.basket = action.payload
-        },
-        removeItem: (state, action) => {
-            const { productId, quantity } = action.payload;
-            const itemIndex = state.basket?.items.findIndex(i => i.productId === productId);
-            if (itemIndex === -1 || itemIndex === undefined) return;
-            state.basket!.items[itemIndex].quantity -= quantity;
-            if (state.basket?.items[itemIndex].quantity === 0) {
-                state.basket.items.splice(itemIndex, 1);
-            }
         }
     },
     extraReducers: (builder => {
@@ -54,8 +56,26 @@ export const basketSlice = createSlice({
         builder.addCase(addBasketItemAsync.rejected, (state) => {
             state.status = 'idle';
         });
+        builder.addCase(removeBasketItemAsync.pending, (state, action) =>{
+            state.status = 'pendingRemoveItem' + action.meta.arg.productId + action.meta.arg.name;
+        });
+        builder.addCase(removeBasketItemAsync.fulfilled, (state, action) => {
+            const { productId, quantity } = action.meta.arg;
+            const itemIndex = state.basket?.items.findIndex(i => i.productId === productId);
+            if (itemIndex === -1 || itemIndex === undefined) return;
+            state.basket!.items[itemIndex].quantity -= quantity;
+            if (state.basket?.items[itemIndex].quantity === 0) {
+                state.basket.items.splice(itemIndex, 1);
+            }
+
+            state.status = 'idle';
+            
+        });
+        builder.addCase(removeBasketItemAsync.rejected, (state) => {
+            state.status = 'idle';
+        });
     })
 });
 
 
-export const { setBasket, removeItem } = basketSlice.actions;
+export const { setBasket } = basketSlice.actions;
--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -44,7 +44,7 @@ export default function ProductCard({product}: Props) {
             <CardActions>
                 <LoadingButton 
                 loading={status.includes('pendingAddItem' + product.id)} 
-                onClick={() => dispatch(addBasketItemAsync({productId: product.id}))} 
+                onClick={() => dispatch(addBasketItemAsync({productId: product.id, quantity: 1}))} 
                 size="small">Add to cart</LoadingButton>
                 <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
             </CardActions>
