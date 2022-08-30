da4fb5efc124110440f51646c0aac58a72dfb6ee
alexmonteil <alex.monteil@outlook.com>
2022-08-29 23:02:52 -0500

    Added UserDto

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 using API.Dtos;
 using API.Entities;
+using API.Services.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,6 +10,7 @@ public class AccountController : BaseApiController
 {
 
     private readonly UserManager<User> _userManager;
+    private readonly ITokenService _tokenService;
 
     public AccountController(UserManager<User> userManager)
     {
@@ -16,7 +18,7 @@ public class AccountController : BaseApiController
     }
 
     [HttpPost("login")]
-    public async Task<ActionResult<User>> Login(LoginDto loginDto) 
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto) 
     {
         var user = await _userManager.FindByNameAsync(loginDto.UserName);
 
@@ -25,7 +27,11 @@ public class AccountController : BaseApiController
             return Unauthorized();
         }
 
-        return user;
+        return new UserDto 
+        {
+            Email = user.Email,
+            Token = await _tokenService.GenerateToken(user)
+        };
     }
 
     [HttpPost("register")]
